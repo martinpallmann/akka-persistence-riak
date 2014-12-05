a144a76477e2f2d944a93b22fd6be097e1227433
Martin Pallmann <sayhello@martinpallmann.de>
2014-12-05 21:48:45 +0100

    move riak setup to travis.yml

--- riak-setup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-sudo riak-admin bucket-type create journal '{"props": {}}'
-sudo riak-admin bucket-type activate journal
-sudo riak-admin bucket-type create journal-seqnrs '{"props":{"datatype":"set"}}'
-sudo riak-admin bucket-type activate journal-seqnrs 
-

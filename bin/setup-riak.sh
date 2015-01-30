0e35ecb82003ba418a3901e5659b46a8d58baf16
Martin Pallmann <sayhello@martinpallmann.de>
2015-01-30 19:47:58 +0100

    put riak setup into script

---
+++ bin/setup-riak.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+sudo riak-admin bucket-type create journal '{"props": {}}'
+sudo riak-admin bucket-type activate journal
+sudo riak-admin bucket-type create journal-seqnrs '{"props":{"datatype":"set"}}'
+sudo riak-admin bucket-type activate journal-seqnrs
